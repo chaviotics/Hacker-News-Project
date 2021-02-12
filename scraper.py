9a32315bbf04dbf717171a0b7638949df4e53f63
Chav Sumalinog <chaviotics@gmail.com>
2021-02-12 19:46:09 +0800

    Added function: create_custom_hn.

--- scraper.py
+++ scraper.py
@@ -1,3 +1,29 @@
-import requests
-from bs4 import BeautifulSoup
+'''
+Grabs the link of the story that has more than 100 votes
+'''
 
+import requests # allows us to download initially the html
+from bs4 import BeautifulSoup # use the html to scrape; cleans up the data
+
+response = requests.get('https://news.ycombinator.com/news')
+# print(response.text)
+
+# CSS selector allows us to grab elements in a HTMl page
+soup = BeautifulSoup(response.text, 'html.parser')
+links = soup.select('.storylink')
+votes = soup.select('.score')
+# we need the links and votes
+
+# print(links)
+# print(votes)
+
+def create_custom_hn(links, votes):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        hn.append(title)
+
+    return hn
+
+print(create_custom_hn(links, votes))
+ 
